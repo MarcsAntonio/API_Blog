19d5b14c034dee6789ff3d947b280a17c6b6122c
Marcos Trujillo <marcostrujillo15@gmail.com>
2023-11-17 12:44:30 -0400

    Creacion de modelo en mongoose y creacion de rutas de pruebas

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const {conexion} = require("./BasedeDatos/conexion")
+const { conexion } = require("./BasedeDatos/conexion");
+const express = require("express");
+const cors = require("cors");
+
 
 console.log("Hola como estas");
 
+// Conectar a la DB
 conexion()
+
+// crear el servidor Node
+const app = express();
+const port = 3900;
+
+// Configurar el Cors
+app.use(cors());
+
+// Convertir el body a objeto js
+app.use(express.json());
+
+// Crear rutas
+app.get("/probando", (req, res)=>{
+    console.log("Se a ejecutado el EndPoind probando")
+    return res.status(200).send(
+    {
+        curso: "master en React",
+        autor:"Victor Roble Web",
+        url: "Victorroblesweb.es/master-react"
+    }
+    )
+})
+
+app.get("/", (req, res)=>{
+    return res.status(200).send(
+    `<div>
+        <h1>Probando ruta 1</h1>
+        <p>Primera prueba de la Ruta 1 Ejemplo</p>
+    </div>`
+    )
+})
+
+// Crear servidor y escuchar peticiones http
+app.listen(port, ()=>{
+    console.log("Salida del servidor por el puerto " + port);
+});
